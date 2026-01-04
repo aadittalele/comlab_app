ec6a6889ac8476981c4294297d3c0bfe08ebfb16
aadittalele <aadittalele@gmail.com>
2026-01-04 07:01:38 -0500

    image clip issue fixed

--- app/orgs/page.tsx
+++ app/orgs/page.tsx
@@ -294,7 +294,7 @@ export default function OrganizationsPage() {
                     id="image"
                     accept="image/*"
                     onChange={handleImageChange}
-                    className="mt-1 block w-0 text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
+                    className="mt-1 block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
                   />
                 </div>
 
